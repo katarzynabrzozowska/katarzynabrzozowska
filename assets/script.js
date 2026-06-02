be85d523da3ad4e1a6fbf25b83418d9a02227f1f
katarzynabrzozowska <katarzynabrzozowskapoland@gmail.com>
2026-06-02 23:30:13 +0200

    Fix of membership and contact

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
-document.querySelectorAll('a[href^="#"]').forEach(link=>{link.addEventListener('click',e=>{const id=link.getAttribute('href');const target=document.querySelector(id);if(target){e.preventDefault();target.scrollIntoView({behavior:'smooth',block:'start'});}})});
+document.querySelectorAll('a[href^="#"]').forEach(link=>{
+  link.addEventListener('click',e=>{
+    const id=link.getAttribute('href');
+    const target=document.querySelector(id);
+    if(target){
+      e.preventDefault();
+      target.scrollIntoView({behavior:'smooth',block:'start'});
+    }
+  });
+});
